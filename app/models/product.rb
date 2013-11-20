54cf20fb0e6bdcebf85a0b87fa741f436172d227
Cori Allen <coriallen@gmail.com>
2013-11-20 16:40:05 -0500

    Cleanup before pull.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,9 @@ class Product < ActiveRecord::Base
 	has_many :reviews
 
 	def tweets
-		TwitterClient.user_timeline(self.twitter_name).map(&:text)
+		Rails.cache.fetch(["tweets", self.twitter_name], expires_in: 30.seconds) do
+			TwitterClient.user_timeline(self.twitter_name).map(&:text)
+		end
 	end
 
 end
