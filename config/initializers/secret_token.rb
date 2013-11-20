172c3a45decfeed1c9f72d8a1fb703bc49c25509
David Fisher <tibbon@gmail.com>
2013-11-20 14:57:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FullCachingExample::Application.config.secret_key_base = '01cd4970712d133c498ea1985404e24f9f00bc85c5117fad97a6ad62232eb30c3c16162dcd481f3d41013dc19c91c621fc6269923ed1fa368e1c393aab3ad154'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FullCachingExample::Application.config.session_store :cookie_store, key: '_full_caching_example_session'
