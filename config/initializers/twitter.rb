27a0d47fb54344ebf83ab99ebbfeda11406be7b9
Cori Allen <coriallen@gmail.com>
2013-11-20 15:45:27 -0500

    Add twitter client in initializer, def tweets method in product.rb

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 TwitterClient = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "TWITTER_CONSUMER_KEY"
-  config.consumer_secret     = "TWITTER_CONSUMER_SECRET"
-  config.access_token        = "TWITTER_ACCESS_TOKEN"
-  config.access_token_secret = "TWITTER_ACCESS_SECRET"
+  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+  config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
 end
